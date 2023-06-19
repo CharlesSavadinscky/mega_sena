d5a3a103d77366e4eb374712e8b5f0823f28f4ed
bernardosfz <132398312+bernardosfz@users.noreply.github.com>
2023-06-19 20:11:45 -0300

    Add files via upload
    Commit 1

---
+++ main.py
@@ -0,0 +1,9 @@
+import random
+
+numeros_escolhidos = []
+
+while True:
+    for i in range(1,7):
+        numeros_escolhidos.append(input("Digite um número de 1 a 60: "))
+
+    print(numeros_escolhidos)
