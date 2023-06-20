30a16db39785ce1b863441c76b0150fcf66c1b77
bernardosfz <132398312+bernardosfz@users.noreply.github.com>
2023-06-20 13:21:21 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-import random
-from twilio.rest import twilio
+import random, numpy as np
 
 numeros_escolhidos = []
 
-while True:
-    for i in range(1,7):
-        numeros_escolhidos.append(int(input("Digite um número de 1 a 60: ")))
+for i in range(1,7):
+    numeros_escolhidos.append(int(input(''+str(i)+ '° Número - Digite um número entre 1 e 60: ')))
+numeros_escolhidos = sorted(numeros_escolhidos)
+print('SEUS NUMEROS: ', numeros_escolhidos)
+input('ENTER PARA COMEÇAR....')
 
-    
-    print(numeros_escolhidos)
+acertos = 0
+
+while acertos != 6:
+    numeros_sorteados = random.sample(range(1,61), 6)
+    numeros_sorteados = sorted(numeros_sorteados)
+    print(numeros_sorteados)
+
+    resultado = np.in1d(numeros_escolhidos, numeros_sorteados)
+    acertos = len(np.intersect1d(numeros_escolhidos, numeros_sorteados))
+
+    if resultado.all():
+        print('PARABENS!!!! VOCE GANHOU')
+        print('NUMEROS SORTEADOS: ', numeros_sorteados)
+        print('SEUS NNUMEROS: ', numeros_escolhidos)
